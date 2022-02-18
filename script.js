b1a82784cf678aaf0041f82e1e3d77e09ae35709
Suraj Prabhu <prabhusuraj103@gmail.com>
2022-02-18 23:40:49 +0530

    Added image and padding

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function animateBar() {
   for (let i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+    var elementVisible = 80;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
@@ -14,31 +14,4 @@ function animateBar() {
   }
 }
 
-function animateDiv() {
-  var reveals = document.querySelectorAll(".intro-container");
-
-  var elementTop = reveals[0].getBoundingClientRect().top;
-  if (elementTop > -8.5) {
-    reveals[0].classList.add("active");
-  } else {
-    reveals[0].classList.remove("active");
-  }
-}
-
-function animateProject() {
-  var reveals = document.querySelectorAll(".project-container");
-
-  var windowHeight = window.innerHeight;
-  var elementTop = reveals[0].getBoundingClientRect().top;
-  var elementVisible = 250;
-
-  if (elementTop < windowHeight - elementVisible) {
-    reveals[0].classList.add("active");
-  } else {
-    reveals[0].classList.remove("active");
-  }
-}
-
-window.addEventListener("scroll", animateDiv);
 window.addEventListener("scroll", animateBar);
-window.addEventListener("scroll", animateProject);
